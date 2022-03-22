20b3eb8da750763670abf0511ba389be34640af6
Jakaran <80308625+jaskaranproductions@users.noreply.github.com>
2022-03-22 17:57:09 +0530

    first commit

---
+++ src/firebase-init.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA98R8U_a5z5htwmUwf7HskSZEWVBuILjQ",
+  authDomain: "todo-e9ecb.firebaseapp.com",
+  projectId: "todo-e9ecb",
+  storageBucket: "todo-e9ecb.appspot.com",
+  messagingSenderId: "532353016651",
+  appId: "1:532353016651:web:da73c6cc0321bbc419bece",
+};
+
+const app = initializeApp(firebaseConfig);
